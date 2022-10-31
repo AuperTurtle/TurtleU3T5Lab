8304d951e5c02a8f008dfdab5e8c7b978020ae48
1E10 Laptop <sample@bths.edu>
2022-10-31 10:56:50 -0400

    .

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+// CODE SEGMENT B
+        int p = 5;
+        int q = 0;
+
+        if ((p < 3 && (p / q) == 2))
+        {
+            System.out.println("it’s true :)");
+        }
+        else
+        {
+            System.out.println("it’s false :(");
+        }
+
+
+    }
+}
